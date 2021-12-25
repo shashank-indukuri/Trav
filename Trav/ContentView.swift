db52e04af94a073a4a1857ba56fe6edaa568147a
shashank-indukuri <shashank.indukuri05@gmail.com>
2021-12-25 17:06:50 -0500

    Initial Commit

---
+++ Trav/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trav
+//
+//  Created by Shashank Indukuri on 12/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trav/TravApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravApp.swift
+//  Trav
+//
+//  Created by Shashank Indukuri on 12/25/21.
+//
+
+import SwiftUI
+
+@main
+struct TravApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
